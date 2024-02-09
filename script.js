27608f3dbf0abf2d173caff0e5b4309e2cfb9ad1
stirlo <3212918+stirlo@users.noreply.github.com>
2024-02-09 19:24:28 +1100

    Update script.js
    reflowedddd and reflowed

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.addEventListener('mousemove', (e) => {
-const card = document.getElementById('card');
-const centerX = window.innerWidth / 2;
-const centerY = window.innerHeight / 2;
-const mouseX = e.clientX - centerX;
-const mouseY = e.clientY - centerY;
-const rotateY = mouseX * 0.1;
-const rotateX = -mouseY * 0.1;
+    const card = document.getElementById('card');
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const mouseX = e.clientX - centerX;
+    const mouseY = e.clientY - centerY;
+    const rotateY = mouseX * 0.1;
+    const rotateX = -mouseY * 0.1;
 
-card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 });
+
 // Add click event listener to document
-    document.addEventListener('click', () => {
-        window.location.href = 'https://tfp.la/stirling/';
-    });
+document.addEventListener('click', () => {
+    window.location.href = 'https://tfp.la/stirling/';
+});
 
-    // Add keydown event listener to document
-    document.addEventListener('keydown', () => {
-        window.location.href = 'https://tfp.la/stirling/';
-    });
-
+// Add keydown event listener to document
+document.addEventListener('keydown', () => {
+    window.location.href = 'https://tfp.la/stirling/';
+});
