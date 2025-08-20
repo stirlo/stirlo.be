5ba25dfa687be11c6e1f03c84d3d0c507e38291c
stirling campbell <3212918+stirlo@users.noreply.github.com>
2025-08-20 18:31:19 +1000

    Update script.js
    trying to fix weird three issues

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* ---------- 0.  ES-MODULE IMPORTS  ---------- */
+import * as THREE from 'three';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass }     from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+
 /* ---------- 0.  CONFIG  ---------- */
 const DAMPING          = 0.95;
 const MIN_SPEED        = 2;
